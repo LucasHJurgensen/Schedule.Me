9bc033f15a6d21cbda6e2c822193e976aafaac5b
Lucas Henrique Jurgensen <lucas_jurgensen@hotmail.com>
2025-05-10 13:12:55 -0300

    subRegister test to catch the errors removed

--- pages/backend/subRegister.js
+++ pages/backend/subRegister.js
@@ -4,28 +4,8 @@ export function subRegister(subject, course, teacher, dayWeek, hourClass){
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({disciplina: subject, curso: course, professor: teacher, diaSemana: dayWeek, horario: hourClass})
     })
-    .then(response => {
-        return response.text(); // Use .text() para capturar o conteúdo bruto
-    })
-    .then(data => {
-        console.log(data); // Verifique o que está sendo retornado do servidor
-        try {
-            const jsonData = JSON.parse(data); // Tente converter para JSON
-            // Processa o jsonData se for válido
-            if (jsonData.status === 'ok') {
-                alert("Disciplina cadastrada com sucesso!");
-                console.log(jsonData);
-            } else if (jsonData.status === "already exists") {
-                alert("Disciplina ja cadastrada para esse curso! Altere o curso e tente novamente!");
-            } else {
-                alert("Falha no cadastro! Por favor, revise os dados!");
-            }
-        } catch (error) {
-            console.error("Erro ao analisar JSON:", error);
-        }
-    })
-    .catch(error => console.error("Erro:", error));
-   /* .then(response => response.json())
+  
+    .then(response => response.json())
 
     .then(data => {
 
@@ -45,6 +25,6 @@ export function subRegister(subject, course, teacher, dayWeek, hourClass){
         }
     })
 
-    .catch(error => console.error("Erro:", error));*/
+    .catch(error => console.error("Erro:", error));
 
 }
